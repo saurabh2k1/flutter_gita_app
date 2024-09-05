16d421dea7968869fbab57ba5a1e49f89f07cbf7
Saurabh Sharma <saurabh@petronetlng.in>
2024-09-05 17:37:06 +0530

    05-09-2024

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_gita_app/language_provider.dart';
 import 'package:provider/provider.dart';
