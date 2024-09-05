ad58a84e24a45a05437becf7be5ab2dbbc234163
Saurabh Sharma <saurabh@petronetlng.in>
2024-09-05 17:28:49 +0530

    cupertino

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_gita_app/language_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -7,28 +8,52 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Settings'),
+    //   ),
+    //   body: Consumer<LanguageProvider>(
+    //     builder: (context, languageProvider, child) {
+    //       return ListView(
+    //         children: [
+    //           ListTile(
+    //             title: const Text('Preferred Language'),
+    //             subtitle: Text(languageProvider.isEnglish ? 'English' : 'Hindi'),
+    //             trailing: Switch(
+    //               value: languageProvider.isEnglish,
+    //               onChanged: (value) {
+    //                 languageProvider.setLanguage(value);
+    //               },
+    //             ),
+    //           ),
+    //         ],
+    //       );
+    //     },
+    //   ),
+    // );
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Settings'),
+        // leading: Icon(Icons.settings),
       ),
-      body: Consumer<LanguageProvider>(
+      child: Consumer<LanguageProvider>(
         builder: (context, languageProvider, child) {
           return ListView(
             children: [
-              ListTile(
+              CupertinoListTile(
                 title: const Text('Preferred Language'),
-                subtitle: Text(languageProvider.isEnglish ? 'English' : 'Hindi'),
-                trailing: Switch(
+                subtitle: Text(languageProvider.isEnglish? 'English' : 'Hindi'),
+                trailing: CupertinoSwitch(
                   value: languageProvider.isEnglish,
                   onChanged: (value) {
                     languageProvider.setLanguage(value);
                   },
+                  ),
                 ),
-              ),
             ],
           );
         },
-      ),
+      )
     );
   }
 }
